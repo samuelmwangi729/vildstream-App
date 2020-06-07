0a64b306a944b8bcd9c23089abc3cd3b01be2c97
samuel mwangi <samuelmwangi729@gmail.com>
2020-06-07 06:33:58 +0300

    ui

---
+++ lib/elements.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'main.dart';
+class DrawerElements extends StatelessWidget {
+  int i=0;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ListView(
+          children: <Widget>[
+        new UserAccountsDrawerHeader(
+              accountName: Text('samuel mwangi'), accountEmail: Text('samuelmwangi729@gmail.com'),
+             currentAccountPicture:
+            GestureDetector(
+              child: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Icon(Icons.person,size: 70.0,color: Colors.red), 
+              )
+            ),
+            decoration: 
+            BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors:[
+                  Colors.red,
+                  Colors.black,
+                ]
+              )
+            ),
+        ),
+        new InkWell(
+          onTap:(){
+            Navigator.push(context, MaterialPageRoute(builder: (context){
+              return MyApp();
+            }));
+          },
+          child: Center(
+            child: ListTile(
+                  title: Text('Home',style:TextStyle(fontFamily:'Times New Roman',fontWeight: FontWeight.bold)),
+                  leading: Icon(Icons.home,color: Colors.red),
+                  dense: true,
+                ),
+          ),
+        ),
+       Divider(
+          height: 1,
+          color: Colors.red
+        ),
+         new InkWell(
+          onTap:(){
+            // Navigator.of(context).push(MaterialPageRoute(builder: (context){
+            //   return Categories();
+            // },));
+          },
+          child: Center(
+            child: ListTile(
+                  title: Text('Categories',style:TextStyle(fontFamily:'Times New Roman',fontWeight: FontWeight.bold)),
+                  leading: Icon(Icons.list,color: Colors.red),
+                  dense: true,
+                ),
+          ),
+        ),
+        Divider(
+          height: 1,
+          color: Colors.red
+        ),
+         new InkWell(
+          onTap:(){},
+          child: Center(
+            child: ListTile(
+                  title: Text('Properties /Property Type',style:TextStyle(fontFamily:'Times New Roman',fontWeight: FontWeight.bold)),
+                  leading:  SvgPicture.asset('assets/house.svg',
+                 width: 25,
+                 alignment: Alignment.centerLeft,
+                 fit: BoxFit.fitHeight,
+                 ),
+                  dense: true,
+                ),
+          ),
+        ),
+         Divider(
+          height: 1,
+          color: Colors.red
+        ),
+         new InkWell(
+          onTap:(){},
+          child: Center(
+            child: ListTile(
+                  title: Text('Listing',style:TextStyle(fontFamily:'Times New Roman',fontWeight: FontWeight.bold)),
+                  leading:  SvgPicture.asset('assets/menu.svg',
+                 width: 25,
+                 alignment: Alignment.centerLeft,
+                 fit: BoxFit.fitHeight,
+                 ),
+                  dense: true,
+                ),
+          ),
+        ),
+        Divider(
+          height: 1,
+          color: Colors.red
+        ),
+         new InkWell(
+          onTap:(){},
+          child: Center(
+            child: ListTile(
+                  title: Text('Blog',style:TextStyle(fontFamily:'Times New Roman',fontWeight: FontWeight.bold)),
+                   leading:  SvgPicture.asset('assets/location1.svg',
+                 width: 25,
+                 alignment: Alignment.centerLeft,
+                 fit: BoxFit.fitHeight,
+                 ),
+                  dense: true,
+                ),
+          ),
+        ),
+       Divider(
+          height: 1,
+          color: Colors.red
+        ),
+         new InkWell(
+          onTap:(){},
+          child: Center(
+            child: ListTile(
+                  title: Text('Contact Us',style:TextStyle(fontFamily:'Times New Roman',fontWeight: FontWeight.bold)),
+                  leading: Icon(Icons.phone,color: Colors.red),
+                  dense: true,
+                ),
+          ),
+        ),
+        Divider(
+          height: 1,
+          color: Colors.red
+        ),
+        Divider(),
+         new InkWell(
+          onTap:(){},
+          child: Center(
+            child: ListTile(
+                  title: Text('About Us',style:TextStyle(fontFamily:'Times New Roman',fontWeight: FontWeight.bold)),
+                  leading: Icon(Icons.help,color: Colors.red),
+                  dense: true,
+                ),
+          ),
+        ),
+        Divider(),
+         new InkWell(
+          onTap:(){},
+          child: Padding(
+            padding: EdgeInsets.only(top:10,left:100),
+            child: ListTile(
+                  title: Text('Version 1.0',style:TextStyle(fontFamily:'Times New Roman',fontWeight: FontWeight.bold,color:Colors.red)),
+                  dense: true,
+                ),
+          ),
+        ),
+
+          ]
+        ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,288 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'elements.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  bool isLoggedIn=false;
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'VildStream',
+      debugShowCheckedModeBanner: false,
+      color: Colors.pink,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
+        scaffoldBackgroundColor: Colors.white,
+        primarySwatch: Colors.pink,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomeScreen(),
     );
   }
 }
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomeScreen extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Colors.white.withOpacity(0.1),
+      // backgroundColor: Colors.white,
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        title: Text('VildStream',
+        style: TextStyle(
+          fontSize: 30
+        )
         ),
+        centerTitle: false,
+        backgroundColor: Colors.black,
+        actions: <Widget>[
+          new Padding(
+            padding: EdgeInsets.only(right:20),
+            child:Icon(Icons.email)
+          ),
+           new Padding(
+            padding: EdgeInsets.only(right:20),
+            child:Icon(Icons.search)
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      drawer: Drawer(
+       child:  DrawerElements()
+      ),
+      body:SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: <Widget>[
+            ClipPath(
+              clipper: MyClipper(),
+              child:Container(
+             child: Column(
+                children: <Widget>[
+                  Expanded(child: Stack(
+                    children: <Widget>[
+                      Image.asset('assets/banner.jpg',
+                      height: 300,
+                      fit: BoxFit.cover
+                      ),
+                    Positioned(
+                      top: 70,
+                      left: 30,
+                      right: 30,
+                      child: Text(
+                        'VildStream Videos',
+                        style: TextStyle(color: Colors.white,fontSize:40,fontWeight: FontWeight.bold)
+                      )
+                    ),
+                    Positioned(
+                       top: 120,
+                      left: 90,
+                      right: 60,
+                      child: Text('Subscription Based Video Streaming Website',
+                      style:TextStyle(color: Colors.white)
+                      )
+                      )
+                    ]
+                  ))
+                ]
+              ),
+              height: 250,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.bottomRight,
+                  end: Alignment.topLeft,
+                  colors:[
+                    Colors.red,
+                    Colors.black,
+                  ]
+                )
+              ),
+            )
+            ),
+            Column(
+              children: <Widget>[
+                Container(
+                  height: 40,
+                 decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.bottomRight,
+                  end: Alignment.topLeft,
+                  colors:[
+                    Colors.black.withOpacity(.9),
+                    Colors.red.withOpacity(.6),
+                  ]
+                )
+              ),
+                  child:ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: <Widget>[
+                      Padding(
+                        padding:EdgeInsets.only(right:20,bottom:5,top:5) ,
+                        child:InkWell(
+                          onTap:(){},
+                          child: Chip(
+                          label: Text(
+                          'All Videos',
+                          style: TextStyle(fontWeight: FontWeight.bold)
+                        ),
+                        backgroundColor: Colors.yellow,
+                        )
+                        )
+                      ),
+                      Padding(
+                        padding:EdgeInsets.only(right:20,bottom:5,top:5) ,
+                        child:InkWell(
+                          onTap:(){},
+                          child: Chip(
+                          label: Text(
+                          'Trailers',
+                          style: TextStyle(fontWeight: FontWeight.bold)
+                        ),
+                        backgroundColor: Colors.yellow,
+                        )
+                        )
+                      ),
+                      Padding(
+                        padding:EdgeInsets.only(right:20,bottom:5,top:5) ,
+                        child:InkWell(
+                          onTap:(){},
+                          child: Chip(
+                          label: Text(
+                          'Featured Videos',
+                          style: TextStyle(fontWeight: FontWeight.bold)
+                        ),
+                        backgroundColor: Colors.yellow,
+                        )
+                        )
+                      ),
+                      Padding(
+                        padding:EdgeInsets.only(right:20,bottom:5,top:5) ,
+                        child:InkWell(
+                          onTap:(){},
+                          child: Chip(
+                          label: Text(
+                          'My Categories',
+                          style: TextStyle(fontWeight: FontWeight.bold)
+                        ),
+                        backgroundColor: Colors.yellow,
+                        )
+                        )
+                      ),
+                    ],
+                  ),
+                ),
+                //the movies categories
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text('VildStream Categories',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20
+                  )
+                  ),
+                ),
+                SizedBox(height: 2,),
+                Container(
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(.1),
+                        offset:Offset(0.0,4.0),
+                        blurRadius: 10
+                      )
+                    ],
+                  ),
+                  width: double.infinity,
+                  height: 230,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: <Widget>[
+                      Padding(padding: EdgeInsets.all(8),
+                      child: CategoryCard('assets/cat.jpg','Comics'),
+                      ),
+                       Padding(padding: EdgeInsets.only(top:1),
+                      child: CategoryCard('assets/cat1.jpg','Science'),
+                      ),
+                       Padding(padding: EdgeInsets.only(top:1),
+                      child: CategoryCard('assets/cat2.jpg','Automotives'),
+                      ),
+                       Padding(padding: EdgeInsets.only(top:1),
+                      child: CategoryCard('assets/cat3.jpg','Blog'),
+                      ),
+                       Padding(padding: EdgeInsets.only(top:1),
+                      child: CategoryCard('assets/cat4.jpg','Education'),
+                      ),
+                       Padding(padding: EdgeInsets.only(top:1),
+                      child: CategoryCard('assets/cat5.jpg','StreamShots'),
+                      ),
+                       Padding(padding: EdgeInsets.only(top:1),
+                      child: CategoryCard('assets/cat6.jpg','Lifestyle'),
+                      )
+                    ],
+                  )
+                )
+              ],
+            ),
+          ]
+        ),
+      )
     );
   }
+}
+
+class  MyClipper extends CustomClipper<Path>{
+  @override
+  Path getClip(Size size){
+    var path = Path();
+    path.lineTo(0, size.height-80);
+    path.quadraticBezierTo(
+      size.width / 2, size.height,  size.width, size.height-80
+      );
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper){
+    return false;
+  }
+}
+
+Widget CategoryCard(String path,String title){
+  return InkWell(
+    onTap:(){},
+    child:Column(
+      children: <Widget>[
+      Card(elevation: .7,
+        child: Image.asset
+          (
+            path,
+            fit:BoxFit.cover,
+            width:150,
+            height:160
+          )
+      ),
+      SizedBox(height: 1,),
+      Text(title,
+      style: TextStyle(
+        color: Colors.white,
+        fontWeight: FontWeight.bold,
+        fontSize: 20
+      )
+      )
+    ],
+    )
+  );
 }
